9a0a05b07f1b9b487a40089d5183113b8800d743
Marcel Benders <marcel.benders@icloud.com>
2019-10-31 08:31:29 +0100

    Initial Commit

---
+++ Slipways/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slipways
+//
+//  Created by Marcel Benders on 31.10.19.
+//  Copyright © 2019 Marcel Benders. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlipwaysTests/SlipwaysTests.swift
@@ -0,0 +1,34 @@
+//
+//  SlipwaysTests.swift
+//  SlipwaysTests
+//
+//  Created by Marcel Benders on 31.10.19.
+//  Copyright © 2019 Marcel Benders. All rights reserved.
+//
+
+import XCTest
+@testable import Slipways
+
+class SlipwaysTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
