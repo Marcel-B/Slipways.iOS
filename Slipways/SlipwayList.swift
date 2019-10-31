e88279ec8e2ee5cb2fd57dc51226707dd75564b5
Marcel Benders <marcel.benders@icloud.com>
2019-10-31 08:49:44 +0100

    (dev) Add project structure

--- Slipways/SlipwayList.swift
+++ Slipways/SlipwayList.swift
@@ -8,14 +8,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SlipwayList: View {
     var body: some View {
         Text("Hello World")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct SlipwayList_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        SlipwayList()
     }
 }
