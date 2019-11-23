29989768caebf1583e534a48ba7d8c034d6baec9
Marcel Benders <marcel.benders@icloud.com>
2019-11-23 10:08:11 +0100

    (0016) Add search in water list

--- Slipways/Services/DataStore.swift
+++ Slipways/Services/DataStore.swift
@@ -31,7 +31,12 @@ class DataStore: ObservableObject{
         return stations
     }
     
-    func getByExpression(exp: (_ isIncluded: Station) throws -> Bool) -> [Station]{
+    func getWatersByExpression(exp: (_ isIncluded: Water) -> Bool) -> [Water]{
+        getWaters()
+            .filter(exp)
+    }
+    
+    func getStationsByExpression(exp: (_ isIncluded: Station) throws -> Bool) -> [Station]{
         let st = getStations()
         do{
             return try st.filter(exp)
@@ -39,6 +44,13 @@ class DataStore: ObservableObject{
             print(error)
         }
         return [Station]()
+    }
+    
+    func getWaters(filter: String) -> [Water]{
+        getWaters()
+            .filter{ (water) -> Bool in
+                water.longname.lowercased().starts(with: filter.lowercased())
+        }
     }
     
     func getStations(filter: String) -> [Station] {
