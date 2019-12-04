d1ab1614e4934cf5f3b894e411ffece41c7f8a57
Marcel Benders <marcel.benders@icloud.com>
2019-12-04 19:08:18 +0100

    (dev) Update Api Scope

--- Slipways/Services/TokenService.swift
+++ Slipways/Services/TokenService.swift
@@ -24,13 +24,13 @@ class TokenService: TokenProtocol{
     func getToken(completion: @escaping (_ token: Token?) -> Void) {
         debugPrint("Fetch token")
         let contentType = "application/x-www-form-urlencoded"
-        if let url = URL(string: "https://newidentity.qaybe.de/connect/token"){
+        if let url = URL(string: "https://identity.qaybe.de/connect/token"){
             let session = URLSession.shared
             var request = URLRequest(url: url)
             request.httpMethod =  HttpMethod.post
             request.setValue(contentType, forHTTPHeaderField: "Content-Type")
             request.setValue("Basic c2xpcHdheXMuaW9zOmxBZGZhMjMyZiFhc2RmXzUwQXNnSzItMQ==", forHTTPHeaderField:"Authorization")
-            let data = Data( "grant_type=client_credentials&scope=slipways.writer".utf8)
+            let data = Data( "grant_type=client_credentials&scope=slipways.api.reader".utf8)
             let task =  session.uploadTask(with: request, from: data) { data, response, error in
                 if let json = data{
                     let value: Token? = self.serializer.parseObject(data: json)
